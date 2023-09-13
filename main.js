a3abd464a93fc4a0393d4563cc95a50a1706e517
Kunal Umap <kunalgumap@gmail.com>
2023-09-13 23:15:16 +0530

    Modification

--- main.js
+++ main.js
@@ -29,40 +29,48 @@ MORSE_CODE_DICT1 = ['.-', '-...',
                     '-----'];
 let MorseCode = "";
 let arrowCode = "_Click_ ";
+let HTML_For_Arrow_Code = "";
+let HTML_For_Morse = "";
 
 input.addEventListener('change',(e)=>{
     let text = input.value.toUpperCase();
     
     for(var i =0;i<text.length;i++){
         if(text[i] == ' '){
-            MorseCode += "_";
+            HTML_For_Morse += `<div class="subDiv smallDiv">_</div>`;
         }else{
             let code = text[i];
             for(var j = 0;j<36;j++){
                 if(MORSE_CODE_DICT[j] == code){
                     MorseCode += MORSE_CODE_DICT1[j];
                     MorseCode += ' ';
+                    HTML_For_Morse += `
+                    <div class="subDiv">${MORSE_CODE_DICT1[j]}</div>`;
                 }
-                console.log(code, MORSE_CODE_DICT[j])
             }
-            
-            // MorseCode += a;
-            // MorseCode += " ";
         }
     }
 
-    for(var i =0;i<MorseCode.length;i++){
-        if(MorseCode[i]==" "){
-            arrowCode += '→ ';
-        }else if(MorseCode[i] == "."){
-            arrowCode += '↑ ';
-        }else if(MorseCode[i] == "-"){
-            arrowCode += '↓ ';
-        }else if(MorseCode[i] == "_"){
-            arrowCode += '← ';
+    op1.innerHTML = HTML_For_Morse;
+    const MORS1 = document.querySelectorAll('.subDiv');
+    MORS1.forEach(ele => {
+        let eleText = ele.innerText;
+        HTML_For_Arrow_Code += `<div class="subDiv">`;
+        for(var i =0;i<eleText.length;i++){
+            console.log(eleText[i])
+            if(eleText[i] == "."){
+                HTML_For_Arrow_Code += '↑ ';
+            }else if(eleText[i] == "-"){
+                HTML_For_Arrow_Code += '↓ ';
+            }else if(eleText[i] == "_"){
+                HTML_For_Arrow_Code += '← ';
+            }
         }
-    }
-    op1.innerHTML = MorseCode;
-    op2.innerHTML = arrowCode +" _Click_";
-    // ↑ ↓ → ←
+        HTML_For_Arrow_Code += `</div>`;
+        // HTML_For_Arrow_Code += '→ ';
+    
+    });
+
+    op2.innerHTML = HTML_For_Arrow_Code;
+
 });
