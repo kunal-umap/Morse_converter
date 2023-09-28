e62407ebfd83d73bd5b9113101f345e8d7a5152e
Kunal Umap <kunalgumap@gmail.com>
2023-09-28 14:58:12 +0530

    minor Changes

--- main.js
+++ main.js
@@ -29,15 +29,15 @@ MORSE_CODE_DICT1 = ['.-', '-...',
                     '-----'];
 let MorseCode = "";
 let arrowCode = "_Click_ ";
-let HTML_For_Arrow_Code = "";
-let HTML_For_Morse = "";
 
 input.addEventListener('change',(e)=>{
     let text = input.value.toUpperCase();
+    let HTML_For_Arrow_Code = "";
+    let HTML_For_Morse = "";
     
     for(var i =0;i<text.length;i++){
         if(text[i] == ' '){
-            HTML_For_Morse += `<div class="subDiv smallDiv">_</div>`;
+            HTML_For_Morse += `<div class="subDiv1">_</div>`;
         }else{
             let code = text[i];
             for(var j = 0;j<36;j++){
@@ -45,25 +45,28 @@ input.addEventListener('change',(e)=>{
                     MorseCode += MORSE_CODE_DICT1[j];
                     MorseCode += ' ';
                     HTML_For_Morse += `
-                    <div class="subDiv">${MORSE_CODE_DICT1[j]}</div>`;
+                    <div class="subDiv1">${MORSE_CODE_DICT1[j]}</div>`;
                 }
             }
         }
     }
 
     op1.innerHTML = HTML_For_Morse;
-    const MORS1 = document.querySelectorAll('.subDiv');
-    MORS1.forEach(ele => {
+    const MORS1 = document.querySelectorAll('.subDiv1');
+    MORS1.forEach((ele,idx) => {
+        console.log(ele,idx)
         let eleText = ele.innerText;
         HTML_For_Arrow_Code += `<div class="subDiv">`;
+        if(text[idx] != " "){
+            HTML_For_Arrow_Code += `<div class="helpLetter">${text[idx]}</div>`;
+        }
         for(var i =0;i<eleText.length;i++){
-            console.log(eleText[i])
             if(eleText[i] == "."){
                 HTML_For_Arrow_Code += '↑ ';
             }else if(eleText[i] == "-"){
                 HTML_For_Arrow_Code += '↓ ';
             }else if(eleText[i] == "_"){
-                HTML_For_Arrow_Code += '← ';
+                HTML_For_Arrow_Code += '→ ';
             }
         }
         HTML_For_Arrow_Code += `</div>`;
